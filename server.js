b5dc8018e63167388758b7c37ff40b5f77ba2de1
Dino Grgić <dino.grgic1@gmail.com>
2022-11-08 22:04:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
-const http = require('node:http');
+const express = require('express');
+const PORT = process.env.PORT || 5000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+express()
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));

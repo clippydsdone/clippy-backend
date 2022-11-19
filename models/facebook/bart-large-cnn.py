33c1ae728329d096e94bd5d8d0e104db086458f9
Dino Grgić <dino.grgic1@gmail.com>
2022-11-20 00:34:55 +0100

    Cnt

--- models/facebook/bart-large-cnn.py
+++ models/facebook/bart-large-cnn.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 
 if __name__ == '__main__':
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    print(summarizer(sys.argv[1], max_length=130, min_length=30, do_sample=False, truncation=True))
+    print(summarizer(sys.argv[1], max_length=130, min_length=30, do_sample=False))
     sys.stdout.flush()

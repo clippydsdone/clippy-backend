8c137d35bb3effd8cd5ad490624a02cc5507cd21
Dino Grgić <dino.grgic1@gmail.com>
2022-11-28 21:00:10 +0100

    Cnt

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ const D3proxy = require('./proxy/d3proxy.js');
 
 app.use(express.json());
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-};
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(allowCrossDomain);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 

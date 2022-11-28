4e8f9854e3bd84b798c7103ecc7dd5a89d6778ad
Dino Grgić <dino.grgic1@gmail.com>
2022-11-28 16:27:05 +0100

    Added try/catch

--- server.js
+++ server.js
@@ -18,32 +18,47 @@ app.get('/', (req, res) => {
 });
 
 app.get('/semantic/paper/id/:id', async (req, res) => {
-  var paperId = req.params.id;
-  var result = await SemanticScholarApi.searchPaperById(paperId);
-  result.data.references = D3proxy.parseReferencesToD3Json(result.data.paperId, result.data.title, result.data.references);
-  res.status(result.status);
-  res.send(result.data);
+  try {
+    var paperId = req.params.id;
+    var result = await SemanticScholarApi.searchPaperById(paperId);
+    result.data.references = D3proxy.parseReferencesToD3Json(result.data.paperId, result.data.title, result.data.references);
+    res.status(result.status);
+    res.send(result.data);
+  }
+  catch (e) {
+    res.send({ "err": e });
+  }
 });
 
 app.post('/semantic/paper/search', async (req, res) => {
-  var query = req.body.query;
-  var response = await SemanticScholarApi.searchPaperIdByKeywoard(query);
-  var paperId = response.data[0].paperId;
-  var result = await SemanticScholarApi.searchPaperById(paperId);
-  result.data.references = D3proxy.parseReferencesToD3Json(result.data.paperId, result.data.title, result.data.references);
-  res.status(result.status);
-  res.send(result.data);
+  try {
+    var query = req.body.query;
+    var response = await SemanticScholarApi.searchPaperIdByKeywoard(query);
+    var paperId = response.data[0].paperId;
+    var result = await SemanticScholarApi.searchPaperById(paperId);
+    result.data.references = D3proxy.parseReferencesToD3Json(result.data.paperId, result.data.title, result.data.references);
+    res.status(result.status);
+    res.send(result.data);
+  }
+  catch (e) {
+    res.send({ "err": e });
+  }
 });
 
 app.post('/semantic/paper/search_multiple', async (req, res) => {
-  var query = req.body.query;
-  var response = await SemanticScholarApi.searchPaperIdByKeywoard(query);
+  try {
+    var query = req.body.query;
+    var response = await SemanticScholarApi.searchPaperIdByKeywoard(query);
 
-  var results = []; 
-  for(var i = 0; i < response.data.length; i++) {
-    var curr = await SemanticScholarApi.searchPaperById(response.data[i].paperId);
-    results.push(curr);
+    var results = [];
+    for (var i = 0; i < response.data.length; i++) {
+      var curr = await SemanticScholarApi.searchPaperById(response.data[i].paperId);
+      results.push(curr);
+    }
+    results.data = results;
+    res.send(results.data);
   }
-  results.data = results;
-  res.send(results.data);
+  catch (e) {
+    res.send({ "err": e });
+  }
 });

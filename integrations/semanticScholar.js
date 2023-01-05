5e787738af3843a571758892ccb30183b482e022
Dino Grgić <dino.grgic1@gmail.com>
2023-01-05 14:03:39 +0100

    Added opened access pdf field

--- integrations/semanticScholar.js
+++ integrations/semanticScholar.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const baseURLSemantic = 'https://api.semanticscholar.org';
 
-async function searchPaperById(paperId, fields = "title,references,paperId,externalIds,url,title,abstract,venue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,fieldsOfStudy,s2FieldsOfStudy,publicationVenue,publicationTypes,publicationDate,journal,citationStyles,authors,tldr") {
+async function searchPaperById(paperId, fields = "paperId,externalIds,url,title,abstract,venue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,openAccessPdf,fieldsOfStudy,publicationTypes,publicationDate,journal,citationStyles,authors,tldr") {
     const getReq = baseURLSemantic + '/graph/v1/paper/' + paperId;
     var result = { "status": 200, "data": null, "fields": fields }
     await axios({

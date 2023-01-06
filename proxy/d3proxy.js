3622180eaf0c62879a17763f7ab4c3b889c0ba7e
Dino Grgić <dino.grgic1@gmail.com>
2023-01-07 00:41:43 +0100

    Add opened access pdf url to response (#16)
    * Added opened access pdf field
    
    * Fixed bug
    
    * Trying out
    
    * Finished base64 functionallity
    
    * Added get paper by id route
    
    * Changed route to GET
    
    * Fixed code smells
    
    * Fixed cors error

--- proxy/d3proxy.js
+++ proxy/d3proxy.js
@@ -1,16 +1,16 @@
 function parseReferencesToD3Json(paperId, paperTitle, references) {
-    var ret = {
+    let ret = {
         "nodes": [{ "id": paperId, "group": 1, "title": paperTitle }],
         "links": []
     }
 
-    for (var i = 0; i < references.length; i++) {
-        let id = references[i].paperId;
-        if (id == undefined){
-            id = references[i].title;
+    for (const element of references) {
+        let id = element.paperId;
+        if (id == undefined) {
+            id = element.title;
         }
-        ret.nodes.push({ "id": id, "group": 2, "title": references[i].title  });
-        ret.links.push({ "source": paperId, "target": id, "group": 2, "distance": 50})
+        ret.nodes.push({ "id": id, "group": 2, "title": element.title });
+        ret.links.push({ "source": paperId, "target": id, "group": 2, "distance": 50 })
     }
 
     return ret;

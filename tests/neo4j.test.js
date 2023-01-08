ed76158c27bdae06dd5d8b7b425cfe48074f6848
Dino Grgić <dino.grgic1@gmail.com>
2023-01-08 02:24:28 +0100

    Added hugging face tests

---
+++ tests/neo4j.test.js
@@ -0,0 +1,16 @@
+jest.setTimeout(30 * 1000);
+
+describe('Neo4j Knowledge Graph', () => {
+
+    test('TB08 - Add references to Neo4j database exists', async () => {
+    });
+
+    test('TB09 - Add references to Neo4j database non existing', async () => {
+    });
+
+    test('TB10 - Get references from Neo4j depth 1', async () => {
+    });
+
+    test('TB11 - Get references from Neo4j of variable depth', async () => {
+    });
+});
